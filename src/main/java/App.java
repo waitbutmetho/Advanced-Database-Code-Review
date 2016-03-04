5214df3d86fc7122b6918c83e39ff6091df5213a
Josh <silversevenfox@yahoo.com>
2016-03-04 11:19:55 -0800

    all tests passing

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -10,5 +10,13 @@ public class App {
   public static void main(String[] args) {
     staticFileLocation("/public/");
     String layout = "templates/layout.vtl";
+
+    get("/", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/index.vtl");
+      model.put("bands", Band.all());
+      model.put("venues", Venue.all());
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
   }
 }
