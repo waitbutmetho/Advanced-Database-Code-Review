e55b4f3bf97b830a5253c593ed9465adafbead7e
Josh <silversevenfox@yahoo.com>
2016-03-04 11:58:53 -0800

    add new routes and vtl's

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -44,5 +44,102 @@ public class App {
       model.put("template", "templates/index.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
+
+    get("/band/:id", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      int id = Integer.parseInt(request.params("id"));
+      Band myBand = Band.find(id);
+      model.put("band", myBand);
+      model.put("template", "templates/band.vtl");
+      model.put("venues", Venue.all());
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    post("/band/update/:id", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      int id = Integer.parseInt(request.params("id"));
+      Band myBand = Band.find(id);
+      int venueId = Integer.parseInt(request.queryParams("venue_id"));
+      Venue myVenue = Venue.find(venueId);
+      myBand.addVenue(myVenue);
+      model.put("band", myBand);
+      model.put("template", "templates/band.vtl");
+      model.put("venues", Venue.all());
+      return new ModelAndView(model, layout);
+      }, new VelocityTemplateEngine());
+
+    post("/band/update/name/:id", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      int id = Integer.parseInt(request.params("id"));
+      Band myBand = Band.find(id);
+      String inputName = request.queryParams("name");
+      myBand.update(inputName);
+      model.put("band", myBand);
+      // model.put("template", "templates/band.vtl"); //page wont redirect to same page
+      model.put("venues", Venue.all());
+      // return new ModelAndView(model, layout);
+      // }, new VelocityTemplateEngine());
+      response.redirect("/");
+      return null;
+    });
+
+
+    post("/band/delete/:id", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      int id = Integer.parseInt(request.params("id"));
+      Band myBand = Band.find(id);
+      myBand.delete();
+      model.put("band", myBand);
+        response.redirect("/");
+        return null;
+      });
+
+    get("/venue/:id", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      int id = Integer.parseInt(request.params("id"));
+      Venue myVenue = Venue.find(id);
+      model.put("venue", myVenue);
+      model.put("template", "templates/venue.vtl");
+      model.put("bands", Band.all());
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    post("/venue/update/:id", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      int id = Integer.parseInt(request.params("id"));
+      Venue myVenue = Venue.find(id);
+      int bandId = Integer.parseInt(request.queryParams("band_id"));
+      Band myBand = Band.find(bandId);
+      myVenue.addBand(myBand);
+      model.put("venue", myVenue);
+      model.put("template", "templates/venue.vtl");
+      model.put("bands", Band.all());
+      return new ModelAndView(model, layout);
+      }, new VelocityTemplateEngine());
+
+    post("/venue/update/name/:id", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      int id = Integer.parseInt(request.params("id"));
+      Venue myVenue = Venue.find(id);
+      String inputName = request.queryParams("name");
+      myVenue.update(inputName);
+      model.put("venue", myVenue);
+      // model.put("template", "templates/band.vtl"); //page wont redirect to same page
+      model.put("bands", Band.all());
+      // return new ModelAndView(model, layout);
+      // }, new VelocityTemplateEngine());
+      response.redirect("/");
+      return null;
+    });
+
+    post("/venue/delete/:id", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      int id = Integer.parseInt(request.params("id"));
+      Venue myVenue = Venue.find(id);
+      myVenue.delete();
+      model.put("venue", myVenue);
+        response.redirect("/");
+        return null;
+      });
   }
 }
